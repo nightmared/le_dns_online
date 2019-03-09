dac65dc75a2839f813e1b4780ab0cb8fa4a35241
Simon THOBY <simonthoby@live.fr>
2019-03-09 17:07:47 +0100

    separation between the DNS library and the acme.sh client

--- lib/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![feature(try_trait)]
-
-mod api;
-pub use crate::api::*;
-pub mod net;
-pub mod error;
-mod bind;
-
-pub static API_URL: &'static str = "https://api.online.net/api/v1";
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::time::SystemTime;
 
-use lib::*;
+use dns_online::*;
 
 fn usage() {
     eprintln!("Usage:\tle_dns_online add_record ONLINE_API_KEY RECORD_NAME TXT_VALUE");
@@ -32,20 +32,30 @@ fn main() {
                 let mut zone_entries = domain.get_zone_records(&zone).unwrap();
                 zone_entries.push(Record::new(record.clone(), net::DNSType::TXT, txt_value, 86400));
                 let new_zone_name = format!("LE-challenge-{}", current_time);
+                println!("Adding a new record to zone {} for domain {}...", zone.name, domain.name);
                 let new_zone = domain.add_version(&new_zone_name).unwrap();
                 domain.set_zone_entries(&new_zone, &zone_entries).unwrap();
                 domain.enable_version(&new_zone).unwrap();
+                println!("The entry {} has been deployed.", record);
             },
             "delete_record" => {
-                let zone_entries: Vec<Record> = domain.get_zone_records(&zone)
-                    .unwrap()
+                println!("Suppressing the entry {} in domain {}...", record, domain.name);
+                let old_zone_entries: Vec<Record> = domain.get_zone_records(&zone).unwrap();
+                let new_zone_entries: Vec<Record> = old_zone_entries
+                    .clone()
                     .into_iter()
-                    .filter(|x| !(x.name == record && x.data == txt_value))
+                    .filter(|x| !(x.record_type == net::DNSType::TXT && x.name == record && x.data[1..x.data.len()-1] == txt_value))
                     .collect();
+                // nothing changed, so we don't do nothing
+                if new_zone_entries.len() == old_zone_entries.len() {
+                    println!("No such entries found, exiting...");
+                    return;
+                }
                 let new_zone_name = format!("LE-challenge-cleanup-{}", current_time);
                 let new_zone = domain.add_version(&new_zone_name).unwrap();
-                domain.set_zone_entries(&new_zone, zone_entries.as_slice()).unwrap();
+                domain.set_zone_entries(&new_zone, new_zone_entries.as_slice()).unwrap();
                 domain.enable_version(&new_zone).unwrap();
+                println!("The entry {} has been destroyed.", record);
             },
             _ => {
                 eprintln!("Invalid action");
