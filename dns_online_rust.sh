d03359b073205c5d5b2ffc81609506a859d43af4
Simon THOBY <simonthoby@live.fr>
2019-03-13 20:48:43 +0100

    fix the shell script

--- dns_online_rust.sh
+++ dns_online_rust.sh
@@ -3,11 +3,11 @@
 export ONLINE_API_KEY="<YOUR API KEY HERE>"
 
 #Usage: dns_myapi_add _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
-dns_online_add() {
+dns_online_rust_add() {
     dnsapi/le_dns_online add_record LE-challenge-$(date +%s) "$ONLINE_API_KEY" $1 $2
 }
 
 #Usage: dns_myapi_rm _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
-dns_online_rm() {
+dns_online_rust_rm() {
     dnsapi/le_dns_online delete_record LE-challenge-done-$(date +%s) "$ONLINE_API_KEY" $1 $2
 }
