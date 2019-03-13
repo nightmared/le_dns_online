9888b0c44a0f0923f3c22239f09adb8bf5525947
Simon THOBY <simonthoby@live.fr>
2019-03-13 20:37:28 +0100

    move the gestion of the zone name to the shelle script

--- dns_online_rust.sh
+++ dns_online_rust.sh
@@ -4,10 +4,10 @@ export ONLINE_API_KEY="<YOUR API KEY HERE>"
 
 #Usage: dns_myapi_add _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_online_add() {
-    dnsapi/le_dns_online add_record "$ONLINE_API_KEY" $1 $2
+    dnsapi/le_dns_online add_record LE-challenge-$(date +%s) "$ONLINE_API_KEY" $1 $2
 }
 
 #Usage: dns_myapi_rm _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_online_rm() {
-    dnsapi/le_dns_online delete_record "$ONLINE_API_KEY" $1 $2
+    dnsapi/le_dns_online delete_record LE-challenge-done-$(date +%s) "$ONLINE_API_KEY" $1 $2
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::env;
-use std::time::SystemTime;
 
 use dns_online::*;
 
 fn usage() {
-    eprintln!("Usage:\tle_dns_online add_record ONLINE_API_KEY RECORD_NAME TXT_VALUE");
-    eprintln!("\t\tle_dns_online delete_record ONLINE_API_KEY RECORD_NAME TXT_VALUE");
+    eprintln!("Usage:\tle_dns_online add_record ZONE_NAME ONLINE_API_KEY RECORD_NAME TXT_VALUE");
+    eprintln!("\t\tle_dns_online delete_record ZONE_NAME ONLINE_API_KEY RECORD_NAME TXT_VALUE");
 }
 
 fn main() {
     let mut args = env::args().skip(1);
-    if args.len() != 4 {
-        eprintln!("Called with an invalid number of arguments (4 expected, received {})", args.len());
+    if args.len() != 5 {
+        eprintln!("Called with an invalid number of arguments (5 expected, received {})", args.len());
         usage();
         return;
     }
     let action = args.next().unwrap();
+    let zone_name = args.next().unwrap();
     let api_key = args.next().unwrap();
     let mut record = args.next().unwrap();
     if !record.ends_with(".") {
@@ -24,16 +24,14 @@ fn main() {
     let txt_value = args.next().unwrap();
 
     let available_domains = query_available_domains(&api_key).unwrap();
-    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
     if let Some((domain, _)) = Domain::find_and_extract_path(&record, available_domains) {
         let zone = domain.get_current_zone().unwrap();
         match action.as_str() {
             "add_record" => {
                 let mut zone_entries = domain.get_zone_records(&zone).unwrap();
                 zone_entries.push(Record::new(record.clone(), net::DNSType::TXT, txt_value, 86400));
-                let new_zone_name = format!("LE-challenge-{}", current_time);
                 println!("Adding a new record to zone {} for domain {}...", zone.name, domain.name);
-                let new_zone = domain.add_version(&new_zone_name).unwrap();
+                let new_zone = domain.add_version(&zone_name).unwrap();
                 domain.set_zone_entries(&new_zone, &zone_entries).unwrap();
                 domain.enable_version(&new_zone).unwrap();
                 println!("The entry {} has been deployed.", record);
@@ -51,8 +49,7 @@ fn main() {
                     println!("No such entries found, exiting...");
                     return;
                 }
-                let new_zone_name = format!("LE-challenge-cleanup-{}", current_time);
-                let new_zone = domain.add_version(&new_zone_name).unwrap();
+                let new_zone = domain.add_version(&zone_name).unwrap();
                 domain.set_zone_entries(&new_zone, new_zone_entries.as_slice()).unwrap();
                 domain.enable_version(&new_zone).unwrap();
                 println!("The entry {} has been destroyed.", record);
