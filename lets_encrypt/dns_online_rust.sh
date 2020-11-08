e733467f75ea2d8ea7de28e057fed1b9f5cc2f97
Simon THOBY <git@nightmared.fr>
2020-11-08 16:14:57 +0100

    update the script for acme.sh to use the new syntax

--- lets_encrypt/dns_online_rust.sh
+++ lets_encrypt/dns_online_rust.sh
@@ -4,10 +4,10 @@ export ONLINE_API_KEY="<YOUR API KEY HERE>"
 
 #Usage: dns_myapi_add _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_online_rust_add() {
-    dnsapi/le_dns_online -o add -z LE-challenge-$(date +%s) -a $ONLINE_API_KEY -n $1 -d $2
+    dnsapi/le_dns_online -a $ONLINE_API_KEY -n $1 --value $2 add --version-name LE-challenge-$(date +%s)
 }
 
 #Usage: dns_myapi_rm _acme-challenge.www.domain.com  "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_online_rust_rm() {
-    dnsapi/le_dns_online -o delete -z LE-challenge-done-$(date +%s) -a $ONLINE_API_KEY -n $1 -d $2
+    dnsapi/le_dns_online -a $ONLINE_API_KEY -n $1 --value $2 delete --version-name LE-challenge-done-$(date +%s)
 }
